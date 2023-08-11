2e2197832dedaa1395ee8069fb9ba3668a1e350f
Mujju-palaan <mdmujahid97@gmail.com>
2023-08-11 18:04:12 +0530

    hearder & footer done

---
+++ src/app/components/Header.js
@@ -0,0 +1,23 @@
+import styles from "@/app/styles/navbar.module.css"
+import Link from "next/link";
+import Image from "next/image";
+import Nav from "@/app/components/Nav";
+
+const Header = () => {
+    const style = {
+        fontFamily: 'ui-sans-serif'
+      }; 
+    return (
+        <header className={styles.main_header}>
+            <div className={styles.navbar_brand}>
+                <Link href="/">
+                    <h1 style={style}> SEHAT</h1>
+                </Link>
+
+            </div>
+            <Nav/>
+        </header>
+    );
+};
+
+export default Header;
---
+++ src/app/contactus/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Contactus = () => {
+  return (
+    <>
+    <div>Contactus</div>
+    
+    
+    </>
+  )
+}
+
+export default Contactus
