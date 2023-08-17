a00a9280629c2b2a847db5994cfa00d7999c7c9d
Mujju Palaan <mdmujahid97@gmail.com>
2023-08-17 10:38:34 +0530

    changes

--- src/app/Container/ProductPage/ProductPage.js
+++ src/app/Container/ProductPage/ProductPage.js
@@ -1,18 +1,19 @@
 "use client"
 import Card from "@/app/components/Card/Card"
+import MotionCard from "@/app/components/Card/MotionCard/MotionCard"
 import PriceItemCard from "@/app/components/Card/PriceItemCard/PriceItemCard"
 
 const ProductPage = () => {
   return (
     <>
       <h1>Product Page</h1>
-      <div style={{display: "flex",  flexWrap:'wrap'}}>
+      <div style={{display: "flex",  flexWrap:'wrap', justifyContent:'center', alignItems:'center'}}>
         <Card></Card>
         <Card></Card>
         <Card></Card>
         <Card></Card>
       </div>
-      <div style={{display: "flex",  flexWrap:'wrap'}}>
+      <div style={{display: "flex",  flexWrap:'wrap', justifyContent:'center', alignItems:'center'}}>
       <PriceItemCard></PriceItemCard>
       <PriceItemCard></PriceItemCard>
       <PriceItemCard></PriceItemCard>
@@ -21,6 +22,7 @@ const ProductPage = () => {
       <PriceItemCard></PriceItemCard>
       <PriceItemCard></PriceItemCard>
       </div>
+      <MotionCard/>
     </>
   )
 }
---
+++ src/app/components/Card/MotionCard/MotionCard.js
@@ -0,0 +1,43 @@
+import { motion } from "framer-motion"
+import { useState } from "react"
+import './MotionCard.css'
+
+const MotionCard = () => {
+    const [isOpen, setIsOpen] = useState(false);
+  return (
+    
+    <>
+        <div className='App'>
+            <motion.div 
+            transition={{layout: {duration: 1, type:'spring'}}}
+            layout 
+            onClick={() => setIsOpen(!isOpen)} 
+            className='card'
+            style={{borderRadius:'2rem', boxShadow:'0px 10px 30px rgba(0,0,0,0.5)'}}
+            >
+                <motion.h2 layout='position'>  
+                    Framer Motion
+                </motion.h2>
+                {isOpen && (
+                <motion.div 
+                initial={{opacity: 0}}
+                animate={{opacity: 1}}
+                transition={{duration: 1}}
+                className="expand">
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam accusantium harum a iste fugit maiores 
+                        necessitatibus ea rerum dolores obcaecati.
+                    </p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, eos.</p>
+                    <button> Read More</button>
+                </motion.div>
+                
+                )}
+
+            </motion.div>
+
+        </div>
+    </>
+  )
+}
+
+export default MotionCard
